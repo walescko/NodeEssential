6b45c7b168559fb32f4a359714ebbed5bf92e050
walescko <walescko@gmail.com>
2023-11-29 06:58:12 -0300

    js - map filter reduce

--- chapter01-02/mapFilterReduce.js
+++ chapter01-02/mapFilterReduce.js
@@ -1,8 +1,18 @@
 //Funções que auxiliam o uso o Arrays
-let numbers00 = [1, 2, 3 , 4, 5]
+let numbers00 = [1, 2, 3, 4, 5]
 
 //map function
 let numbers01 = numbers00.map(n => n*2)
 console.log(numbers01)
 
+//filter function
+let numbers02 = numbers00.filter(n => n > 2)
+console.log(numbers02)
 
+//reduce function
+//sem elemento inicial do array informado.
+let s = numbers00.reduce((accumulatedValue, n) => accumulatedValue + n*2)
+console.log(s)
+//com elemento inicial do array informado
+s = numbers00.reduce((accumulatedValue, n) => accumulatedValue + n*2, 0)
+console.log(s)
