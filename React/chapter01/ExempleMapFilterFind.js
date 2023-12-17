3106ce8a3e55a298adcc05b17f335502cb2d5a48
walescko <walescko@gmail.com>
2023-12-17 05:17:24 -0300

    filter

--- React/chapter01/ExempleMapFilterFind.js
+++ React/chapter01/ExempleMapFilterFind.js
@@ -1,7 +1,12 @@
 let books00 = [ "CSS3", "Javascript", "Python", "HTML", "Node", "React" ]
+const books03 = [
+    {title: "Construindo sites com HTML", author: "Mauricio Samy Silva"},
+    {title: "Web Scraping com Python", author: "Ryan Mitchell"},
+    {title: "CSS3", author: "Mauricio Samy Silva"}
+]
 console.log(books00)
 console.log(books00.length)
-console.log("map()")
+console.log("Sessão usando map()")
 let books01 = books00.map((book) => "Livro " + book)
 let books02 = books00.map((book, index) => "Livro " + index + " " + book)
 
@@ -9,11 +14,7 @@ console.log(books00)
 console.log(books01)
 console.log(books02)
 
-const books03 = [
-    {title: "Construindo sites com HTML", author: "Mauricio Samy Silva"},
-    {title: "Web Scraping com Python", author: "Ryan Mitchell"},
-    {title: "CSS3", author: "Mauricio Samy Silva"}
-]
+
 
 let books04 = books03.map((book) => "Livro: " + book.title)
 let books05 = books03.map((book) => "Autor: " + book.author)
@@ -22,3 +23,13 @@ console.log(books03)
 console.log(books04)
 console.log(books05)
 
+console.log("Sessaõ filter()")
+let book06 = books03.filter((book) => book.title === "CSS3")
+let book07 = books03.filter((book) => book.author === "Mauricio Samy Silva")
+let book08 = books03.filter((book)=> book.title.includes("com"))
+console.log("Filter CSS3")
+console.log(book06)
+console.log("Filter Mauricio Samy Silva")
+console.log(book07)
+console.log("Filter com")
+console.log(book08)
