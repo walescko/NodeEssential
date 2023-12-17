a81e6497bd74e128dd67aac1a8bd483c75315a23
walescko <walescko@gmail.com>
2023-12-17 11:01:39 -0300

    templete literals

---
+++ React/chapter01/templetaliterals.js
@@ -0,0 +1,8 @@
+console.log("Templete Literals")
+const title01 = `Livro React do "Maujor"`
+const price = 80
+const message01 =
+`O preço normal do ${title01} é de R$ ${price},00
+Na promoção o preço cai para R$ ${price*0.8},00`
+
+console.log(message01)
