5f74d031069be93a08a78a0c440a56751c679a22
walescko <walescko@gmail.com>
2023-12-17 21:08:14 -0300

    modules

---
+++ React/chapter01/main.js
@@ -0,0 +1,11 @@
+import toStudy from "./utils.js";
+import {toPraise} from "./utils.js";
+
+const btn1 = document.querySelector("#btn1")
+btn1.addEventListener("click", () => {
+    toStudy("Vamos estudar React")
+})
+const btn2 = document.querySelector("#btn2")
+btn2.addEventListener("click", () => {
+    toPraise("O livro React do Maujor é muito bom")
+})
---
+++ React/chapter01/utils.js
@@ -0,0 +1,5 @@
+const toStudy = (text) => { console.log(text)}
+const toPraise = (praise) => {console.log(praise)}
+
+export default toStudy
+export {toPraise}
