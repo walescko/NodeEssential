0dbcbd741abf35a84b19d5f7ec6be3f5ca35bd4c
walescko <walescko@gmail.com>
2023-12-28 04:40:19 -0300

    array js -> json

--- node/chapter06b/app.js
+++ node/chapter06b/app.js
@@ -7,9 +7,16 @@ function callback(request, response){
 
     response.writeHead(200, {"Content-Type": "application/json; charset=utf-8"})
     if (path == '/test'){
-        let people = {'nome':'Ricardo','sobrenome':'Lecheta'}
+        let people = []
+        let people01 = {'name':'Ricardo','surename':'Lecheta'}
+        let people02 = {'name': 'Linus', 'surname': 'Torvalds'}
+
+        people.push(people01)
+        people.push(people02)
+
         let json = JSON.stringify(people)
         response.end(json)
+
     } else {
         response.end("Path not found: " + path)
     }
