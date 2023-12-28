c6bd38e7f444b150084c6c64b7bce96d3cab2f5a
walescko <walescko@gmail.com>
2023-12-28 04:36:10 -0300

    json content type and js -> json

---
+++ node/chapter06b/app.js
@@ -0,0 +1,20 @@
+let http = require('http')
+let url = require('url')
+
+function callback(request, response){
+    let parts = url.parse(request.url)
+    let path = parts.path
+
+    response.writeHead(200, {"Content-Type": "application/json; charset=utf-8"})
+    if (path == '/test'){
+        let people = {'nome':'Ricardo','sobrenome':'Lecheta'}
+        let json = JSON.stringify(people)
+        response.end(json)
+    } else {
+        response.end("Path not found: " + path)
+    }
+}
+
+let server = http.createServer(callback)
+server.listen(3000)
+console.log("Servidor inicialized in http://localhost:3000/")
