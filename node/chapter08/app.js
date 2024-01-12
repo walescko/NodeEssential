d5c321b62d8d1071a543735500844ac24458d362
walescko <walescko@gmail.com>
2024-01-12 18:53:00 -0300

    js-postman - post method

--- node/chapter08/app.js
+++ node/chapter08/app.js
@@ -1,7 +1,8 @@
 //import CarroDB from "./CarroDB.js"
-const CarroDB = require('./CarroDB.js')
 let http = require('http')
 let url = require('url')
+
+const CarroDB = require('./CarroDB.js')
 
 function getCarros(response, tipo){
     CarroDB.getCarrosByTipo(tipo, function (carros){
@@ -10,18 +11,41 @@ function getCarros(response, tipo){
     })
 }
 
+function salvarCarro(response, carro){
+    CarroDB.save(carro, function (carro){
+        console.log("Carro salvo com sucesso: " + carro.id)
+        let json = JSON.stringify(carro)
+        response.end(json)
+    })
+}
+
 function callback(request, response){
     let parts = url.parse(request.url)
     let path = parts.path
     response.writeHead(200, {"Content-Type": "application/json; charset=utf-8"})
-    if (path == '/carros/classicos'){
-        getCarros(response, "classicos")
-    } else if (path == '/carros/esportivos'){
-        getCarros(response, "esportivos")
-    } else if (path == '/carros/luxo'){
-        getCarros(response, "luxo")
-    } else {
-        response.end("Not found: " + path)
+    if(request.method == "GET") {
+        if (path == '/carros/classicos') {
+            getCarros(response, "classicos")
+        } else if (path == '/carros/esportivos') {
+            getCarros(response, "esportivos")
+        } else if (path == '/carros/luxo') {
+            getCarros(response, "luxo")
+        } else {
+            response.end("Not found: " + path)
+        }
+    } else if (request.method == "POST") {
+        let body = ''
+        request.on('data', function(data){
+            body += data
+        })
+        request.on('end', function (){
+            // response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"})
+            console.log("POST Body" + body)
+            let carro = JSON.parse(body)
+            salvarCarro(response, carro)
+            // response.end("POST Body: " + body)
+        })
+        return
     }
 }
 
