b537ef273662c266358cfd8d48996edc4aaad10f
walescko <walescko@gmail.com>
2024-01-13 06:47:45 -0300

    js express - hello world

---
+++ node/chapter09/app.js
@@ -0,0 +1,12 @@
+let express = require('express')
+let app = express()
+
+app.get('/', function(req, res) {
+    res.send("Hello World Express")
+})
+
+let server = app.listen(3000, function() {
+    let host = server.address().address
+    let port = server.address().port
+    console.log("Servidor iniciado em http://%s:%s", host, port)
+})
