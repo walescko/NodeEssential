78810f32835d4a146d2c02642279dab03cebc066
walescko <walescko@gmail.com>
2024-01-23 04:22:48 -0300

    js - query parameters

--- node/chapter09/app.js
+++ node/chapter09/app.js
@@ -1,9 +1,14 @@
 let express = require('express')
 let app = express()
 
-app.get('/', function(req, res) {
-    // res.send("Hello World Express")
-    res.json({nome: "Walescko", sobrenome: "Chimendes"})
+app.get('/pessoa', function(req, res) {
+    let  nome = req.query.nome
+    let sobrenome = req.query.sobrenome
+    // res.json({nome: "Walescko", sobrenome: "Chimendes"})
+    res.status(200).type("text")
+    // res.send("Hello World Express 2")
+    res.send(nome + " " + sobrenome)
+    // res.json({msg: 'ops...'})
 })
 
 let server = app.listen(3000, function() {
