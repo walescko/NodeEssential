d8625a088decdf5cf639d3100c7e8b0a0a0fb54b
walescko <walescko@gmail.com>
2024-02-04 21:52:13 -0300

    js - get tipo/all cars

--- node/chapter10/app.js
+++ node/chapter10/app.js
@@ -3,6 +3,16 @@ let app = express()
 app.get('/', function (req, res) {
     res.send("CARS API")
 })
+
+app.get('/carros', function (req, res){
+    res.send("Lista de todos os carros aqui.")
+})
+
+app.get('/carros/:tipo', function(req, res){
+    let tipo = req.params.tipo
+    res.send("Lista dos carros: " + tipo)
+})
+
 let server = app.listen(3000, function(){
     let host = server.address().address
     let port = server.address().port
