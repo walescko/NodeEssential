697d091d58abb9f268d2c9313645796f1678c0b1
walescko <walescko@gmail.com>
2024-02-09 04:41:19 -0300

    js - post put export

--- node/chapter10/routes/carros.js
+++ node/chapter10/routes/carros.js
@@ -22,3 +22,27 @@ router.delete('/carro/:id(\\d+), ', function(req, res){
         res.json({msg: 'Carro deletado com sucesso.'})
     })
 })
+
+router.get('/carros/:tipo', function (req, res){
+    let tipo = req.params.tipo
+    CarroDB.getCarrosByTipo(tipo, function (carros){
+        res.json(carros)
+    })
+})
+
+router.post('/carros', function (req, res ){
+    let carro = req.body
+    CarroDB.save(carro, function (carro){
+        res.json(carro)
+    })
+})
+
+router.put('/carro', function(req, res){
+    let carro = req.body
+    CarroDB.update(carro, function (carro){
+        res.json({msg: 'Carro atualizado com sucesso.'})
+    })
+})
+
+module.exports = router
+
