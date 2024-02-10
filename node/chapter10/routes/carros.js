42da883773508485f9baadf9f94f372866998650
walescko <walescko@gmail.com>
2024-02-10 04:40:00 -0300

    js - api routes cars

--- node/chapter10/routes/carros.js
+++ node/chapter10/routes/carros.js
@@ -2,20 +2,20 @@ let express = require('express')
 const router = express.Router()
 const CarroDB = require('../model/CarroDB.js')
 
-router.get('/carros', function (req, res){
+router.get('/', function (req, res){
     CarroDB.getCarros(function (carros){
         res.json(carros)
     })
 })
 
-router.get('/carros/:id(\\d_)', function(req, res){
+router.get('/:id(\\d_)', function(req, res){
     let id = req.params.id
     CarroDB.getCarroById(id, function(carro){
         res.json(carro)
     })
 })
 
-router.delete('/carro/:id(\\d+), ', function(req, res){
+router.delete('/:id(\\d+), ', function(req, res){
     let id = req.params.id
     console.log("Deletar carro " + id)
     CarroDB.deleteById(id, function(affectedRows){
@@ -23,21 +23,21 @@ router.delete('/carro/:id(\\d+), ', function(req, res){
     })
 })
 
-router.get('/carros/:tipo', function (req, res){
+router.get('/:tipo', function (req, res){
     let tipo = req.params.tipo
     CarroDB.getCarrosByTipo(tipo, function (carros){
         res.json(carros)
     })
 })
 
-router.post('/carros', function (req, res ){
+router.post('/', function (req, res ){
     let carro = req.body
     CarroDB.save(carro, function (carro){
         res.json(carro)
     })
 })
 
-router.put('/carro', function(req, res){
+router.put('/', function(req, res){
     let carro = req.body
     CarroDB.update(carro, function (carro){
         res.json({msg: 'Carro atualizado com sucesso.'})
