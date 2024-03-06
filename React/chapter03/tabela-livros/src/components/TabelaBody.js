9784deca5a95144c98c1e9266e04f0f1b7e81ff7
walescko <walescko@gmail.com>
2024-03-06 17:34:04 -0300

    js-react - fix

--- React/chapter03/tabela-livros/src/components/TabelaBody.js
+++ React/chapter03/tabela-livros/src/components/TabelaBody.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TabelaBody = props => (
+const TabelaBody = (props) => (
     <tbody>
         {props.livros.map((livro, index)=> (
             <tr key={livro.id}>
@@ -8,7 +8,7 @@ const TabelaBody = props => (
                 <td>{livro.titulo}</td>
                 <td>{livro.autor}</td>
                 <td><button className="botao remover"
-                onClick={ () => props.removerLinha(livro.id)}>Remover</button></td>
+                onClick={ () => props.removerLinha(livro.id)} id={livro.id}>Remover</button></td>
             </tr>
         ))}
     </tbody>
