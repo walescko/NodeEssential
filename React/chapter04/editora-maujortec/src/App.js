c5dc38e684e1d7da05667f58c3b8079eda451e64
walescko <walescko@gmail.com>
2024-03-09 04:43:02 -0300

    react - update maujortec

---
+++ React/chapter04/editora-maujortec/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+//import {Fragment} from "react";
+
+function App() {
+  return (
+    <>
+      <header className="topo">Topo</header>
+      <main className="principal">Principal</main>
+      <footer className="rodape">Rodapé não dá chule!</footer>
+    </>
+  );
+}
+
+export default App;
