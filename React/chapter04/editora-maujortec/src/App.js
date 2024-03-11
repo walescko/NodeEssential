495fe4d63eeff6f933833d61a9ae4f0877617a52
walescko <walescko@gmail.com>
2024-03-10 21:52:43 -0300

    react

--- React/chapter04/editora-maujortec/src/App.js
+++ React/chapter04/editora-maujortec/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="topo">
+          <h1 className="logo"></h1>
+          <ul>
+              <li><a href="">Navagação</a></li>
+          </ul>
       </header>
-    </div>
+      <main className="principal">
+          <h2>Últimos Lançamentos</h2>
+          <div className="card">Card</div>
+      </main>
+      <footer className="rodape">
+          <p>Conteúdos cedidos pela Editora Novatec &#8212; Copyrigth 2020</p></footer>
+
+    </>
   );
 }
 
