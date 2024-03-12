69437169f1896130b7a0c52f908e1064acdb39b7
walescko <walescko@gmail.com>
2024-03-12 07:10:42 -0300

    react

--- React/chapter05/editora-maujortec/src/App.js
+++ React/chapter05/editora-maujortec/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+
 // import './App.css';
 import React from "react";
+import Logo from "./components/Logo.js";
 
 function App() {
-  return (
-    <>
-      <header className="topo">
-          <h1 className="logo"></h1>
-          <ul>
-              <li><a href="">Navagação</a></li>
-          </ul>
-      </header>
-      <main className="principal">
-          <h2>Últimos Lançamentos</h2>
-          <div className="card">Card</div>
-      </main>
-      <footer className="rodape">
-          <p>Conteúdos cedidos pela Editora Novatec &#8212; Copyrigth 2020</p></footer>
-
-    </>
-  );
+  return <Logo />
 }
 
 export default App;
---
+++ React/chapter05/editora-maujortec/src/components/Navegacao.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Navegacao = () => {
+    return (
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#"></a>Frontend</li>
+            <li><a href="#"></a>Programação</li>
+            <li><a href="#"></a>Desing</li>
+            <li><a href="#"></a>Catálogo</li>
+        </ul>
+    )
+}
+export default Navegacao
