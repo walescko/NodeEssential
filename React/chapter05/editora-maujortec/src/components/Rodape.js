7d128f0770ec8a60c55b6597af8a90727e36ba52
walescko <walescko@gmail.com>
2024-03-13 20:48:01 -0300

    react - rodape

---
+++ React/chapter05/editora-maujortec/src/components/Rodape.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Rodape = () => {
+    return (
+        <footer className="rodape">
+            <p>Conteúdos cedidos pela Editora Novatec &#8212; &copy; Copyright 2020</p>
+        </footer>
+    )
+}
+export default Rodape;
