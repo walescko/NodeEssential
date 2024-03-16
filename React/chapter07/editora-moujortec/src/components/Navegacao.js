6a108dbec1709f651a5ba9c8a0adea805f503f0f
walescko <walescko@gmail.com>
2024-03-16 04:57:59 -0300

    js error

---
+++ React/chapter07/editora-moujortec/src/components/Navegacao.js
@@ -0,0 +1,20 @@
+import React from "react"
+import {NavLink} from "react-router-dom"
+
+let linkCorrente = {
+    color: "#027399"
+}
+
+const Navegacao = () => {(
+    
+        <ul>
+            <li><NavLink exact activeStyle={linkCorrente} to="/">Home</NavLink></li>
+            <li><NavLink exact activeStyle={linkCorrente} to="/frontend">Frontend</NavLink></li>
+            <li><NavLink exact activeStyle={linkCorrente} to="/programacao">Programação</NavLink></li>
+            <li><NavLink exact activeStyle={linkCorrente} to="/desing">Desing</NavLink></li>
+            <li><NavLink exact activeStyle={linkCorrente} to="/catalogo">Catálogo</NavLink></li>
+        </ul>
+    
+)}
+
+export default Navegacao
